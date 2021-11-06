e8cbeab714dbfc59f68ab7bcca32ecfed8af3b39
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-06 04:17:48 -0700

    add get all dojos

--- flask_app/controllers/dojos_controller.py
+++ flask_app/controllers/dojos_controller.py
@@ -5,79 +5,59 @@
 from flask_app import app
 from flask import render_template, session, redirect, request, flash
 import flask_app
-from flask_app.models import users_class
+from flask_app.models import dojos_model
 
 # //// SHOW /////////////////////////////////////
 
-@app.route('/dojos/')
-@app.route('/dojos')                                                         # Main Page
-def dojos():
-    print("******** in dojos main page *******************")
-    return render_template("dojos.html")
-
-@app.route('/')                                                         # Main Page
+@app.route('/')                                                     # Main Page
 def index():
-    return redirect("/dojos")
-
-@app.route('/users/new')                                                # **** FORM **** Create 1 New Users Page
-def users_new():
-    print("******** in New Users *******************")
-    return render_template("create.html")
-
-@app.route('/users/<int:id>/update')                                      # **** FORM **** Edit 1 User Page
-def users_id_updqte(id):
-    print("********* In Users ID Edit ****************")
-    data = {
-        'id': id
-    }
-    user = users_class.Users.get_one(data)                                          # Retrieve an instance of the user with the given ID
-    return render_template("users_id_update.html", user=user)
+    return redirect("/dojos")                                       # Redirects to get all the dojos page
 
 # //// CREATE ////////////////////////////////////
 
-@app.route('/users/new/post', methods=['POST'])                         # Retrieve the input values from create form
-def users_new_post():
-    print("**** In Users New Post Retrieval **************")
+@app.route('/dojos/post', methods=['POST'])                             # Retrieve the input values from create form
+def dojos_post():
+    print("**** In Dojos Post **************")
     data = {                                                            # Create Data Dictionary from values in form
-        'first_name': request.form['first_name'],
-        'last_name': request.form['last_name'],
-        'email': request.form['email']
+        'name': request.form['name'],
     }
+    print("Date is:")
     print(data)
-    users_class.Users.save(data)                                        # Insert data retrieved into users database
-    return redirect("/users")
+    dojos_model.Dojos.save(data)
+    return redirect("/dojos")
 
 # //// RETRIEVE ////////////////////////////////////
 
-@app.route('/users/')
-@app.route('/users')                                                    # Read All Users Page
-def users():
-    print("**** Retrieving Users *******************")
-    all_users = users_class.Users.get_all()                             # Get all instances of users from the database
-    return render_template("read_all.html", all_users = all_users)
+# **** GET ALL *************************************
+@app.route('/dojos/')
+@app.route('/dojos')                                                    # Read All Page
+def dojos():
+    print("**** Retrieving Dojos *******************")
+    all_dojos = dojos_model.Dojos.get_all()                             # Get all instances of from the database
+    return render_template("dojos.html", all_dojos = all_dojos)
 
-@app.route('/users/<int:id>')                                           # Retrive the data from one specified user
-def users_id (id):
-    print ("*********** In users id ******************")
-    data = {
-        'id': id
-    }
-    user = users_class.Users.get_one(data)
-    return render_template("users_read_one.html", user=user)
+# @app.route('/users/<int:id>')                                           # Retrive the data from one specified user
+# def users_id (id):
+#     print ("*********** In users id ******************")
+#     data = {
+#         'id': id
+#     }
+#     user = users_class.Users.get_one(data)
+#     return render_template("users_read_one.html", user=user)
 
 # //// UPDATE ////////////////////////////////////
 
-@app.route('/users/<int:id>/update/post', methods=['POST'])             # Update a specified user's information
-def users_id_update_post(id):
-    print ("*********** In Users ID Edit POST *****************")
-    data = {                                                            # retrieve the data from the form
-        'id': id,
-        'first_name': request.form['first_name'],
-        'last_name': request.form['last_name'],
-        'email': request.form['email']
-    }
-    users_class.Users.update_one(data)
-    return redirect('/users')
+# @app.route('/users/<int:id>/update/post', methods=['POST'])             # Update a specified user's information
+# def users_id_update_post(id):
+#     print ("*********** In Users ID Edit POST *****************")
+#     data = {                                                            # retrieve the data from the form
+#         'id': id,
+#         'first_name': request.form['first_name'],
+#         'last_name': request.form['last_name'],
+#         'email': request.form['email']
+#     }
+#     users_class.Users.update_one(data)
+#     return redirect('/users')
 
 # //// DELETE ////////////////////////////////////
 
@@ -87,7 +67,7 @@ def users_id_delete(id):
     data = {
         'id': id
     }
-    users_class.Users.delete(data)
+    dojos_model.Dojos.delete(data)
     return redirect('/users')
 
 # //// 404 CATCH //////////////////////////////////
---
+++ flask_app/models/dojos_model.py
@@ -0,0 +1,63 @@
+# import the function that will return an instance of a connection ////////
+from flask_app.config.mysqlconnection import connectToMySQL
+
+TARGETDATABASE = 'dojos_and_ninjas_schema'                              # Designates the database we are using
+TABLENAME = "dojos"                                                     # Designates the table we are using
+
+# //// USERS CLASS ////////////////////////////////////////////////////////
+class Dojos:
+    def __init__( self , data ):                                        # Constructor function
+        self.id = data['id']
+        self.name = data['name']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    # //// CREATE //////////////////////////////////////////////////////////
+
+    # **** Insert One Method ***********************************************
+    # @returns ID of created user
+    @classmethod
+    def save(cls, data ):
+        query = "INSERT INTO " + TABLENAME +" (name) VALUES ( %(name)s );"
+        # data is a dictionary that will be passed into the save method from server.py
+        return connectToMySQL(TARGETDATABASE).query_db( query, data )
+        
+    # //// RETRIEVE /////////////////////////////////////////////////////////
+
+    # **** Get All Class Method *******************************************
+    # @Returns: a list of instances of the class
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM " + TABLENAME + ";"
+        results = connectToMySQL(TARGETDATABASE).query_db(query)        # Call the connectToMySQL function with the target db
+        list_of_instances = []                                          # Initialize an empty list where we can store instances of the class
+        for class_instance in results:                                  # Iterate over the db results and create instances of the cls objects
+            list_of_instances.append( cls(class_instance) )             # Add each instance of the class to the list of instances
+        return list_of_instances
+    
+    # **** Get One Class Method *******************************************
+    # @Returns: an instance of the class
+    @classmethod
+    def get_one(cls, data:dict):
+        query = "SELECT * FROM " + TABLENAME +" WHERE id = %(id)s;"
+        results = connectToMySQL(TARGETDATABASE).query_db(query, data)  # Call the connectToMySQL function with the target db
+                                                                        # result is a list of a single dictionary
+        return cls(results[0])                                          # return an instance of the dictionary
+
+    # //// UPDATE //////////////////////////////////////////////////////////
+
+    # **** Update One Class Method *****************************************
+    # @Returns: Nothing
+    @classmethod
+    def update_one(cls, data:dict):
+        query = "UPDATE " + TABLENAME +" SET first_name=%(first_name)s, last_name=%(last_name)s, email=%(email)s WHERE id=%(id)s"
+        return connectToMySQL(TARGETDATABASE).query_db(query, data)
+
+    # //// DELETE //////////////////////////////////////////////////////////
+
+    # **** Delete One Class Method *****************************************
+    # @Returns: Nothing
+    @classmethod
+    def delete(cls, data:dict):
+        query = "DELETE FROM " + TABLENAME + " WHERE id=%(id)s"
+        return connectToMySQL(TARGETDATABASE).query_db(query, data)
